7af3d770082032f2ea0377ad6bd68256a7209dee
Aditya Parab <contact.adityaparab@gmail.com>
2019-04-23 12:57:04 +0200

    removed build output from scm

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var MemeBot_1 = require("./MemeBot");
-var memeBot = new MemeBot_1.MemeBot();
-memeBot.run();
-//# sourceMappingURL=index.js.map
